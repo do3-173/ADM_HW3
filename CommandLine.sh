1c8731b2c9c7b9b079363807fa7608c0ec57a5b2
do3_173 <edo.fejzic@hotmail.com>
2023-11-19 20:17:29 +0100

    [CLQ] Done

--- CommandLine.sh
+++ CommandLine.sh
@@ -3,12 +3,13 @@ for i in {2..6000}
 do
    tail -n +2 TSV/course_${i}.tsv >> merged_courses.tsv
 done
-echo -n "Country: "
-cut -f11 merged_courses.tsv | sort | uniq -c | sort -nr | head -1 | awk '{count=$1; $1=""; sub(/^ /,""); print $0 ", number of occurrences: " count}'
-echo -n "City: "
-cut -f10 merged_courses.tsv | sort | uniq -c | sort -nr | head -1 | awk '{count=$1; $1=""; sub(/^ /,""); print $0 ", number of occurrences: " count}'
-part_time=$(awk -F'\t' 'tolower($X) ~ /part[- ]time/ {print $2}' merged_courses.tsv | sort | uniq | wc -l)
-echo "Number of universities that give part time: ${part_time}" 
-total_courses=$(wc -l < merged_courses.tsv)
+echo -n "Country that offers most Master's Degrees: "
+cut -f11 merged_courses.tsv | sort | uniq -c | sort -nr | head -1 | awk '{print substr($0, index($0,$2)) ", number of occurrences: " $1}'
+echo -n "City that offers most Master's Degrees: "
+cut -f10 merged_courses.tsv | sort | uniq -c | sort -nr | head -1 | awk '{print substr($0, index($0,$2)) ", number of occurrences: " $1}'
+part_time=$(awk -F'\t' 'tolower($0) ~ /part[- ]time/ {print $2}' merged_courses.tsv | sort | uniq | wc -l)
+echo "Number of universities that offer part-time courses: ${part_time}"
+total_courses=$(awk -F'\t' 'NR > 1 && $1 != ""' merged_courses.tsv | wc -l)
 engineering_courses=$(awk -F '\t' 'tolower($1) ~ /engineer/' merged_courses.tsv | wc -l)
-echo "Percentage of engineering courses: $(echo "scale=2; $engineering_courses * 100 / $total_courses" | bc) %"
+percentage=$(echo "scale=2; $engineering_courses * 100 / $total_courses" | bc)
+echo "Percentage of engineering courses: $percentage%"
